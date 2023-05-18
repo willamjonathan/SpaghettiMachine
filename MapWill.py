dd18668538b4ff105625ab87147bdbaace091867
willamjonathan <williamjonathan1289@gmail.com>
2023-05-18 22:24:48 +0700

    "added cond, gak msk akal"

--- MapWill.py
+++ MapWill.py
@@ -11,37 +11,28 @@ ball = sphere(pos=vector(-3, 2, 0), radius=0.5, color=color.red)
 # Obstacles
 box_angle = -30
 
-# box2 = box(pos=vector(-2, 0.5, 0), size=vector(1, 0.1, 3), color=color.yellow)
-
-
 box2 = box(pos=vector(2, -4, 0), size=vector(1, 11, 3), color=color.orange)
-box3 = box(pos=vector(4, -12, 0), size=vector(8, 1, 3), color=color.yellow)
-box4 = box(pos=vector(0, -6, 0), size=vector(1, 12, 3), color=color.yellow)
-box5 = box(pos=vector(12, -14, 0), size=vector(9, 1, 3), color=color.green)
+box3 = box(pos=vector(4, -12, 0), size=vector(8, 1, 3), color=color.green)
+box4 = box(pos=vector(0, -6, 0), size=vector(1, 12, 3), color=color.orange)
+box5 = box(pos=vector(12, -14, 0), size=vector(9, 1, 3), color=color.yellow)
 box5.rotate(angle=radians(box_angle), axis=vector(
     0, 0, 1), origin=vector(11, -13, 0))
-# sin30*9 = depan (box 6); y sekarang di 4.5+14
+
 x_box6_cos30 = (math.sqrt(3)/2) * 9
 x_box6 = x_box6_cos30+11
-# 18.794228634059948
 
 box6 = box(pos=vector(x_box6, -16.5, 0),
            size=vector(8, 1, 3), color=color.green)
 
-box7 = box(pos=vector(25, -24.5, 0), size=vector(1, 28, 3), color=color.green)
-# end di 38.5
-box8 = box(pos=vector(22, -24.5, 0), size=vector(1, 16, 3), color=color.green)
+box7 = box(pos=vector(25, -24.5, 0), size=vector(1, 28, 3), color=color.orange)
+box8 = box(pos=vector(22, -24.5, 0), size=vector(1, 16, 3), color=color.orange)
 
 box8 = box(pos=vector(5, -38.5, 0), size=vector(40, 1, 3), color=color.green)
-# stop box
+
 box9 = box(pos=vector(-5, -37.5, 0), size=vector(2, 2, 2), color=color.red)
 
-
-# slope_ground = box(pos=vector(2, -0.5, 0),
-#                    size=vector(1, 0.1, 2), color=color.orange)
-
 # Set initial velocity
-ball.velocity = vector(1, 0, 0)  # Move to the right
+ball.velocity = vector(1, -1, 0)  # Move to the right
 
 # Define the force of gravity
 g = vector(0, -9.8, 0)
@@ -53,18 +44,35 @@ dt = 0.01
 while True:
     rate(100)
 
-    # # Update position
-    # ball.pos += ball.velocity * dt
+    # Update position
+    ball.pos += ball.velocity * dt
 
-    # # Apply force
-    # ball.velocity += g * dt
+    # Apply gravity
+    ball.velocity += g * dt
 
-    # # Check if ball hits the box2
-    # if ball.pos.y < box2.pos.y + box2.size.y and ball.velocity.y < 0:
-    #     ball.velocity.y = 0  # Apply an upward impulse to the ball
-    #     #  ball.pos  # Place box3 at the position of the ball
+    if ball.pos.y < ground.pos.y + ground.size.y and ball.velocity.y < 0:
+        ball.velocity.y = 0
+        # Move only in X direction
+        ball.pos += ball.velocity * dt
 
-    # # Check if ball hits the slope
-    # if ball.pos.x >= slope.pos.x and ball.pos.z >= slope.pos.z and \
-    #         ball.pos.x <= slope.pos.x + slope.size.x and ball.pos.z <= slope.pos.z + slope.size.z:
-    #     ball.velocity.y = -10  # Apply a downward impulse to the ball
+    # Check if ball hits box2
+    if ball.pos.x >= box2.pos.x - box2.size.x and ball.pos.y >= box2.pos.y - box2.size.y and ball.pos.x <= box2.pos.x + box2.size.x and ball.pos.y >= box3.pos.y + box3.size.y:
+        ball.velocity.y = -1  # Make the ball fall
+        ball.velocity.x = 0
+        # Move only in X direction
+        ball.pos += ball.velocity * dt
+
+# Check if ball hits box3
+    if ball.pos.x >= box3.pos.x - box3.size.x/2 and ball.pos.y <= box3.pos.y + box3.size.y:
+        ball.velocity.y = 0  # Stop the ball in the Y direction
+        ball.velocity.x = 1
+        # Move only in X direction
+        ball.pos += ball.velocity * dt
+
+# Check if ball passes through box3 and hits box5
+    if ball.pos.x >= box5.pos.x - box5.size.x/2 and ball.pos.y >= box5.pos.y - box5.size.y/2:
+        slope_angle = math.radians(box_angle)
+        # Set the ball's velocity for the slope
+        ball.velocity.y = math.sin(slope_angle)-0.8
+
+        ball.pos += ball.velocity * dt
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,28 @@
-# Create a VPython canvas
-scene = canvas(width=800, height=600, background=color.white,
-               center=vector(0, 0, 0), fov=0.5)
+# Set initial velocity
+# Velocity when force is given; moving X direction
+ball.velocityX = vector(1, 0, 0)  # Move to the right
+# Velocity when ball falls;
+ball.velocityY = vector(0, -1, 0)
+
+
+# Define the force of gravity
+g = vector(0, -9.8, 0)
+
+# Time step
+dt = 0.01
+
+# Simulation loop
+while True:
+    rate(100)
+
+    # Update position
+    ball.pos += ball.velocityX * dt + ball.velocityY * dt
+
+    # Apply gravity
+    ball.velocityY += g * dt
+
+    if ball.pos.y < ground.pos.y + ground.size.y and ball.velocityY < 0:
+        ball.velocityY = 0
+
+    # Move only in X direction
+    ball.pos += ball.velocityX * dt
